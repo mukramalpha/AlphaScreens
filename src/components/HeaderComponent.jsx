3ea8c218140667f0890f50bb644ae3ae4373b8f3
mukramalpha <mukram.alphatech@gmail.com>
2024-02-04 18:59:34 +0530

    "modified the code"

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import companyLogo from '../assets/alpha_logo.jpeg'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+
+const HeaderComponent = () => {
+
+
+  
+
+ 
+
+  return (
+    <div>
+        <header>
+        <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+            <div>
+            <img src={companyLogo} alt="Alpha Inc. logo" className='img-test'/> 
+              <a href='https://thealphatech.co.in' className='navbar-brand'>Alpha Tech Solutions</a>
+            </div>
+            <div className='collapse navbar-collapse'>
+               <ul className='navbar-nav'>
+                
+                   <li className='navbar-item'>
+                  <NavLink to='/home' className='nav-link'>Home</NavLink>
+                </li>
+                <li className='navbar-item'>
+                  <NavLink to='/about' className='nav-link'>About</NavLink>
+                </li>
+                  
+               </ul>
+            </div>
+            <ul className='navbar-nav'>
+              <li className='navbar-item'>
+                <NavLink to='/register' className='nav-link'>Register</NavLink>
+              </li>
+             
+                <li className='navbar-item'>
+                <NavLink to='/login' className='nav-link'>Login</NavLink>
+              </li>
+               
+                 
+                 
+                 
+               </ul>
+        </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
